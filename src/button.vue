bc534af9d59b6c067b78a503023811a6166cdb23
zhengbigbig <zhengbigbig@github.com>
2018-09-29 17:15:04 +0800

    验证iconPosition 为left 或者为right

--- src/button.vue
+++ src/button.vue
@@ -11,7 +11,17 @@
 </template>
 <script>
     export default {
-        props:['icon','iconPosition']
+/*        props:['icon','iconPosition']*/
+        props:{
+            icon:[],
+            iconPosition:{
+                type:String,
+                default:'left',
+                validator(value){
+                    return value === 'left' || value === 'right';
+                }
+            }
+        }
     }
 </script>
 <style lang="scss">
