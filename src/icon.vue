f78bb466d7ce1f043ee1761676b6de13c2c853d7
zhengbigbig <zhengbigbig@github.com>
2018-10-01 15:51:01 +0800

    添加 icon  更新 svg.js

--- src/icon.vue
+++ src/icon.vue
@@ -5,12 +5,13 @@
 </template>
 
 <script>
+    import './svg'
     export default {
         props:['name']
     }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
     .g-icon {
         width: 1em;
         height: 1em;
