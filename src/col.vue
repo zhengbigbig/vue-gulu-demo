28dc49f3185983cc364ce9aee313142606ddafea
zhengbigbig <zhengbigbig@github.com>
2018-10-02 17:27:05 +0800

    基本实现row col的功能

---
+++ src/col.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="col" :class="[`col-${span}`,offset && `offset-${offset}`]"
+         :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}"
+         >
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GuluCol",
+        props: {
+            span:{
+                type:[Number,String]
+            },
+            offset:{
+                type:[Number,String]
+            }
+        },
+        data(){
+            return {
+                gutter: 0
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .col {
+        height:100px;
+        background: grey;
+        width: 50%;
+        border: 1px solid red;
+        $class-prefix: col-;
+        @for $n from 1 through 100 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n /100)* 100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 100 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n /100)* 100%;
+            }
+        }
+    }
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row" :style="{marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'}">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GuluRow",
+        props:{
+            gutter: {
+                type:[Number,String]
+            }
+        },
+        mounted(){
+            this.$children.forEach((vm)=>{
+                vm.gutter = this.gutter
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    .row {
+        display: flex;
+    }
+</style>
