23569d0232b21a0a592f44a2364dd579c9106158
zhengbigbig <zhengbigbig@github.com>
2018-10-04 13:52:20 +0800

    实现toast autoClose closeButton enableHtml 多行文字功能

---
+++ src/toast.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="toast" ref="wrapper">
+        <div class="message">
+            <slot v-if="!enableHtml"></slot>
+            <div v-else v-html="$slots.default[0]"></div>
+        </div>
+        <div class="line" ref="line"></div>
+        <span class="close" v-if="closeButton" @click="onClickClose">{{closeButton.text}}</span>
+    </div>
+</template>
+<script>
+    export default {
+        name: "GuluToast",
+        props:{
+            autoClose:{
+                type:Boolean,
+                default:true
+            },
+            autoCloseDelay:{
+                type:Number,
+                default:55
+            },
+            closeButton:{
+                type:Object,
+                default(){
+                    return {
+                        text:'关闭',callback:undefined
+                    }
+                }
+            },
+            enableHtml:{
+                type:Boolean,
+                default:false
+            }
+        },
+        mounted(){
+            this.updateStyles()
+            this.execAutoClose()
+        },
+        methods:{
+            updateStyles(){
+                this.$nextTick(()=>{
+                    this.$refs.line.style.height =
+                        `${this.$refs.wrapper.getBoundingClientRect().height}px`
+                })
+                //style只获取内联元素，而不获取CSS
+            },
+            execAutoClose(){
+                if(this.autoClose){
+                    setTimeout(()=>{
+                        this.close()
+                    },this.autoCloseDelay * 1000)
+                } 
+            },
+            close(){
+                this.$el.remove()
+                this.$destroy()
+            },
+            log(message){
+                alert(message)
+            },
+            onClickClose(){
+                this.close()
+                if(this.closeButton && typeof this.closeButton.callback === 'function'){
+                    this.closeButton.callback(this) //传入toast到回调
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    $font-size:14px;
+    $toast-min-height:40px;
+    $toast-background:rgba(0,0,0,.75);
+    .toast {
+        font-size:$font-size;
+        line-height:1.8;
+        min-height:$toast-min-height;
+        position: fixed;
+        top:0;
+        left:50%;
+        transform: translateX(-50%);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0 16px;
+        color:white;
+        border-radius: 4px;
+        background:$toast-background;
+        box-shadow: 0 0 3px 0 rgba(0,0,0,.5) ;
+        > .line{
+            height:100%;
+            border-left:1px solid #666;
+            margin:0 16px;
+        }
+        > .close{
+        }
+        > .message{
+            padding:8px 0;
+        }
+    }
+</style>
